3a58b3b844bb6ae2567339a72a2e9197ca9e84b3
baki504 <baki504@users.noreply.github.com>
2024-08-12 21:07:14 +0900

    20240818 slide (#1)
    * fix
    
    * update
    
    * update

--- marp.config.mjs
+++ marp.config.mjs
@@ -1,17 +1,18 @@
-const canonicalUrl = process.env.URL || undefined
+const canonicalUrl = process.env.URL || undefined;
 
 /** @type {import('@marp-team/marp-cli').Config} */
 const config = {
   allowLocalFiles: true,
   ogImage: (() => {
-    if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`
+    if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`;
     if (process.env.VERCEL_URL)
-      return `https://${process.env.VERCEL_URL}/og-image.jpg`
-  
-    return undefined
-  })(),
-  themeSet: 'themes',
-  url: canonicalUrl,
-}
+      return `https://${process.env.VERCEL_URL}/og-image.jpg`;
 
-export default config
+    return undefined;
+  })(),
+  themeSet: "themes",
+  url: canonicalUrl,
+  html: true,
+};
+
+export default config;
