e560a63487e12827117ef600e482c563f604a4f4
baki504 <baki504@msn.com>
2024-08-21 06:14:22 +0900

    chore: Generate images from PlantUML files

---
+++ scripts/generate-uml-images.js
@@ -0,0 +1,61 @@
+const plantuml = require("node-plantuml");
+const fs = require("fs");
+const path = require("path");
+
+const inputDir = path.join(__dirname, "..", "plantuml");
+const outputDir = path.join(__dirname, "..", "slides", "assets");
+
+fs.readdir(inputDir, (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err);
+    return;
+  }
+
+  const markdownNames = files.filter((file) => file.endsWith(".md"));
+
+  if (markdownNames.length === 0) {
+    console.log("No markdown files found in the input directory.");
+    return;
+  }
+
+  console.log(`Generating images from [${markdownNames}].`);
+
+  Promise.all(
+    markdownNames.map(
+      (name) =>
+        new Promise((resolve, reject) => {
+          const filePath = path.join(inputDir, name);
+          fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {
+              console.error("Error reading file:", err);
+              reject(err);
+              return;
+            }
+
+            const basename = path.basename(name, path.extname(name));
+            const outputName = path.join(outputDir, `${basename}.png`);
+
+            var writeStream = fs.createWriteStream(outputName);
+
+            writeStream.on("finish", () => {
+              console.log(`Image generated [${outputName}]`);
+              resolve();
+            });
+
+            writeStream.on("error", (err) => {
+              console.error("Error generating image:", err);
+              reject(err);
+            });
+
+            plantuml.generate(data).out.pipe(writeStream);
+          });
+        })
+    )
+  )
+    .then(() => {
+      console.log("All images generated successfully.");
+    })
+    .catch((err) => {
+      console.error("Error generating images:", err);
+    });
+});
