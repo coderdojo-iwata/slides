8077d444049d4213a0c909b5fbc1c10369519fdc
baki504 <baki504@users.noreply.github.com>
2024-08-23 21:09:31 +0900

    Support og image exp (#12)
    * support og-image generation
    
    * fix github pages settings temporary
    
    * Revert "fix github pages settings temporary"
    
    This reverts commit a330df79761b7208bfcffcc224d9101ac5c4a0af.
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- scripts/generate-og-images.js
+++ scripts/generate-og-images.js
@@ -7,32 +7,30 @@ const fs = require("fs");
  *
  * @param {string} basename - The base name of the markdown file.
  */
-const generateOgImage = (basename) => {
+const generateOgImage = async (basename) => {
   console.log(`run og script for ${basename}`);
 
   const markdownPath = path.join(__dirname, "..", "slides", `${basename}.md`);
-  const ogImagePath = path.join(
-    __dirname,
-    "..",
-    "public",
-    "og",
-    `${basename}.png`
-  );
+  const ogImagePath = path.join(__dirname, "..", "public", `${basename}.png`);
 
-  const ogImageGeneration = spawn(
-    "npm",
-    ["run", "marp", "--", markdownPath, "-o", ogImagePath],
-    {
-      stdio: "inherit",
-    }
-  );
+  await new Promise((resolve, reject) => {
+    const ogImageGeneration = spawn(
+      "npm",
+      ["run", "marp", "--", markdownPath, "-o", ogImagePath],
+      {
+        stdio: "inherit",
+      }
+    );
 
-  ogImageGeneration.on("error", (err) => {
-    console.error("Error running script:", err);
-  });
+    ogImageGeneration.on("error", (err) => {
+      console.error("Error running script:", err);
+      reject(err);
+    });
 
-  ogImageGeneration.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
+    ogImageGeneration.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+      resolve();
+    });
   });
 };
 
@@ -43,4 +41,8 @@ const markdownBaseNames = fs
   .filter((fileName) => fileName.endsWith(".md"))
   .map((fileName) => path.basename(fileName, path.extname(fileName)));
 
-Promise.all(markdownBaseNames.map(generateOgImage));
+(async () => {
+  for (const basename of markdownBaseNames) {
+    await generateOgImage(basename);
+  }
+})();
