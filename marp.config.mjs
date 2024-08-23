ed1ed2e805645c07895b6431ddcf806264f26909
baki504 <baki504@msn.com>
2024-08-23 20:09:20 +0900

    update

--- marp.config.mjs
+++ marp.config.mjs
@@ -3,13 +3,13 @@ const canonicalUrl = process.env.URL || undefined;
 /** @type {import('@marp-team/marp-cli').Config} */
 const config = {
   allowLocalFiles: true,
-  ogImage: (() => {
-    if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`;
-    if (process.env.VERCEL_URL)
-      return `https://${process.env.VERCEL_URL}/og-image.jpg`;
+  // ogImage: (() => {
+  //   if (canonicalUrl) return `${canonicalUrl}/og-image.jpg`;
+  //   if (process.env.VERCEL_URL)
+  //     return `https://${process.env.VERCEL_URL}/og-image.jpg`;
 
-    return undefined;
-  })(),
+  //   return undefined;
+  // })(),
   themeSet: "themes",
   url: canonicalUrl,
   html: true,
