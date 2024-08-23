e9085646f001ffd7e22e2750d11148d671297bf6
baki504 <baki504@users.noreply.github.com>
2024-08-23 19:20:34 +0900

    Support og image (#11)
    * support og-image generation
    
    * fix github pages settings temporary
    
    * Revert "fix github pages settings temporary"
    
    This reverts commit a330df79761b7208bfcffcc224d9101ac5c4a0af.

---
+++ scripts/generate-og-images.js
@@ -0,0 +1,46 @@
+const { spawn } = require("node:child_process");
+const path = require("path");
+const fs = require("fs");
+
+/**
+ * Generates Open Graph (OG) images for each markdown file in the specified directory.
+ *
+ * @param {string} basename - The base name of the markdown file.
+ */
+const generateOgImage = (basename) => {
+  console.log(`run og script for ${basename}`);
+
+  const markdownPath = path.join(__dirname, "..", "slides", `${basename}.md`);
+  const ogImagePath = path.join(
+    __dirname,
+    "..",
+    "public",
+    "og",
+    `${basename}.png`
+  );
+
+  const ogImageGeneration = spawn(
+    "npm",
+    ["run", "marp", "--", markdownPath, "-o", ogImagePath],
+    {
+      stdio: "inherit",
+    }
+  );
+
+  ogImageGeneration.on("error", (err) => {
+    console.error("Error running script:", err);
+  });
+
+  ogImageGeneration.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+const markdownDir = path.join(__dirname, "..", "slides");
+
+const markdownBaseNames = fs
+  .readdirSync(markdownDir)
+  .filter((fileName) => fileName.endsWith(".md"))
+  .map((fileName) => path.basename(fileName, path.extname(fileName)));
+
+Promise.all(markdownBaseNames.map(generateOgImage));
